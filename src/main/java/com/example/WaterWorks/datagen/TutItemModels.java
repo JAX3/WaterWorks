0dad179aa564600d4ccee9a167b720bced1db8f3
Drew Milne <thecore95x@gmail.com>
2022-07-05 19:38:14 +0100

    texture fix

--- src/main/java/com/example/WaterWorks/datagen/TutItemModels.java
+++ src/main/java/com/example/WaterWorks/datagen/TutItemModels.java
@@ -17,10 +17,10 @@ public class TutItemModels  extends ItemModelProvider {
 
     @Override
     protected  void registerModels(){
-        withExistingParent(Registration.WATERSTEEL_ORE_OVERWORLD_ITEM.get().getRegistryName().getPath(),modLoc( "blocks/watersteel_ore_end"));
-        withExistingParent(Registration.WATERSTEEL_ORE_nether_ITEM.get().getRegistryName().getPath(),modLoc( "blocks/watersteel_ore_deepslate"));
-        withExistingParent(Registration.WATERSTEEL_ORE_END_ITEM.get().getRegistryName().getPath(),modLoc( "blocks/watersteel_ore_overworld"));
-        withExistingParent(Registration.WATERSTEEL_ORE_DEEPSLATE_ITEM.get().getRegistryName().getPath(),modLoc( "blocks/watersteel_ore_nether"));
+        withExistingParent(Registration.WATERSTEEL_ORE_OVERWORLD_ITEM.get().getRegistryName().getPath(),modLoc( "block/watersteel_ore_end"));
+        withExistingParent(Registration.WATERSTEEL_ORE_nether_ITEM.get().getRegistryName().getPath(),modLoc( "block/watersteel_ore_deepslate"));
+        withExistingParent(Registration.WATERSTEEL_ORE_END_ITEM.get().getRegistryName().getPath(),modLoc( "block/watersteel_ore_overworld"));
+        withExistingParent(Registration.WATERSTEEL_ORE_DEEPSLATE_ITEM.get().getRegistryName().getPath(),modLoc( "block/watersteel_ore_nether"));
     }
 
 
